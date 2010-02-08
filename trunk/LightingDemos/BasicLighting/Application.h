d87a442c6240c56b4c6685265723ce4b7bd15131
ithiliel <ithiliel@1610d384-d83c-11de-a027-019ae363d039>
2010-02-08 17:05:10 +0000

    Comments added

--- trunk/LightingDemos/BasicLighting/Application.h
+++ trunk/LightingDemos/BasicLighting/Application.h
@@ -1,3 +1,11 @@
+//------------------------------------------------------------------------------
+//--
+//--	Base application framework initialising Ogre3D's rendering engine.
+//--
+//--	TODO:	Create a library so the same code doesn't have to be copied and pasted
+//--			throughout eeevery other test project
+//------------------------------------------------------------------------------
+
 #ifndef __APPLICATION__H__
 #define __APPLICATION__H__
 
@@ -26,49 +34,109 @@ namespace Ogre
 }
 
 class SimpleInputManager;
-//-- 
+///
+/// The main application class
+///
 class Application: SimpleInputManager
 {
 public:
+	/// Constructor: Initialise pointers and sets the window name
+	/// @param windowName	name of the window
 	Application(Ogre::String windowName=DEFAULT_WIN_NAME);
+	/// Destructor
 	virtual ~Application();
 
+	/// Initialise the application, as well as all of its subsystems
 	virtual void initialise();
+	/// Start and run the application's game loop
 	virtual void go();
+	/// Cleans up resources, pointers and all the app's subsystems
 	virtual void cleanUp();
 
 	//Event handling
+
+	/// Process a keyboard press event
+	/// @param e	keyboard event
+	/// @return		true if the event has been handled correctly
 	virtual bool keyPressed( const OIS::KeyEvent& e );
+	/// Process a keyboard release event
+	/// @param e	keyboard event
+	/// @return		true if the event has been handled correctly
 	virtual bool keyReleased(const OIS::KeyEvent& e);
+	/// Process a mouse motion event
+	/// @param e	mouse event
+	/// @return		true if the event has been handled correctly
 	virtual bool mouseMoved(const OIS::MouseEvent &e);
+
+	/// Process a mouse click event
+	/// @param e	mouse event
+	/// @return		true if the event has been handled correctly
 	virtual bool mousePressed(const OIS::MouseEvent &e, OIS::MouseButtonID id);
+	/// Process a mouse release event
+	/// @param e	mouse event
+	/// @return		true if the event has been handled correctly
 	virtual bool mouseReleased(const OIS::MouseEvent &e, OIS::MouseButtonID id);
 
 	//Scene configuration
+
+	/// Initialise the scene and all of the entities that'll be displayed
 	virtual void createScene();
+
+	/// Create cameras
 	virtual void createCameras();
+	
+	/// Create viewports
 	virtual void createViewports();
+
+	/// Initialise GUI subsystem using CEGUI
 	virtual void setupCEGUI();
+	
+	///Initialise debug overlays
 	virtual void setupOverlay();
 
 
 	//Game update methods
+	/// Update the game logic
+	/// @param	elapsedTime	time elapsed since last update
 	virtual void updateLogic(float elapsedTime);
+	/// Update the game graphics. This will be used on
+	/// animated scenes
+	/// @param	elapsedTime	time elapsed since last update
 	virtual void updateGraphics(float elapsedTime);
+
+	/// Update the stats that'll be displayed on the debug windows
 	virtual void updateStats();
 
 protected:
 	//Setup methods
+	/// Create Ogre's root object
 	void createRoot();
+
+	/// Define resource locations
 	void defineResources();
+
+	/// Setup the render system: it will launch the
+	/// config dialog
 	void setupRenderSystem();
+
+	/// Create the application's main window
 	void createRenderWindow();
+
+	/// Initialise and load resource groups
 	void initialiseResourceGroups();
+	
+	/// Set up the scene: init cameras, viewports, lights and the scene's entities
 	void setupScene();
+
+	/// Set up the input subsystem
 	void setupInputSystem();
 
 	//Auxiliary methods
+
+	/// Translate the camera
 	virtual void moveCamera();
+
+	/// Capture input and process it asynchronously
 	virtual void getInput();
 
 	bool m_exitRequested;
@@ -87,7 +155,7 @@ protected:
 
 	Ogre::Overlay* m_debugOverlay;
 
-
+	// Attributes to control the camera's movement
 	Ogre::Vector3			m_TranslateVector;
 	Ogre::Real				m_MoveSpeed; 
 	Ogre::Degree			m_RotateSpeed; 
