f0749a8acb8a49eeff7461a7b5d8bf8f20c5b203
ithiliel <ithiliel@1610d384-d83c-11de-a027-019ae363d039>
2010-03-21 00:33:59 +0000

    - Added world existence flags (they're still in the logicComponentWorldExistence, until the LogicComponent has been integrated within the game) - Added scripting-related attributes (state and script filename). The Filename may be changed to a function name. - Added life and health attributes

--- OUAN/OUAN/Src/Logic/LogicComponent/LogicComponent.cpp
+++ OUAN/OUAN/Src/Logic/LogicComponent/LogicComponent.cpp
@@ -14,6 +14,60 @@ void LogicComponent::update(long elapsedTime)
 {
 }
 
+bool LogicComponent::existsInDreams() const
+{
+	return mExistsInDreams;
+}
+void LogicComponent::setExistsInDreams(bool existsInDreams)
+{
+	mExistsInDreams=existsInDreams;
+}
+
+bool LogicComponent::existsInNightmares() const
+{
+	return mExistsInNightmares;
+}
+void LogicComponent::setExistsInNightmares(bool existsInNightmares)
+{
+	mExistsInNightmares=existsInNightmares;
+}
+
+int LogicComponent::getState() const
+{
+	return mState;
+}
+void LogicComponent::setState(int state)
+{
+	mState=state;
+}
+
+int LogicComponent::getHealthPoints() const
+{
+	return mHealthPoints;
+}
+void LogicComponent::setHealthPoints(int healthPoints)
+{
+	mHealthPoints=healthPoints;
+}
+
+int LogicComponent::getNumLives() const
+{
+	return mNumLives;
+}
+void LogicComponent::setNumLives(int numLives)
+{
+	mNumLives=numLives;
+}
+
+std::string LogicComponent::getScriptFilename() const
+{
+	return mScriptFilename;
+}
+void LogicComponent::setScriptFilename(const std::string& scriptFilename)
+{
+	mScriptFilename=scriptFilename;
+}
+
 TLogicComponentParameters::TLogicComponentParameters() : TComponentParameters()
 {
 }
