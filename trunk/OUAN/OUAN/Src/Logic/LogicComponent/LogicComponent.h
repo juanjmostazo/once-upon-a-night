de3bdf4d16e13ac6033502654a76c11bc00e2198
ithiliel <ithiliel@1610d384-d83c-11de-a027-019ae363d039>
2010-03-21 00:33:59 +0000

    - Added world existence flags (they're still in the logicComponentWorldExistence, until the LogicComponent has been integrated within the game) - Added scripting-related attributes (state and script filename). The Filename may be changed to a function name. - Added life and health attributes

--- trunk/OUAN/OUAN/Src/Logic/LogicComponent/LogicComponent.h
+++ trunk/OUAN/OUAN/Src/Logic/LogicComponent/LogicComponent.h
@@ -11,17 +11,48 @@ namespace OUAN
 		~LogicComponent();
 
 		virtual void update(long elapsedTime);
+	private:
+		/// Number of lives of the component
+		int mNumLives;
+		/// Health points of the component
+		int mHealthPoints;
+		/// Logic state of the parent game object
+		int mState;
+		/// Filename of the script that'll be executed by the logic subsystem
+		/// to update this component's parent game object
+		std::string mScriptFilename;
 
-		//Event handlers
-		//void onDoSomething(EventData,emitter, ...);
-		//void onDoSomethingElse(...);
+		bool mExistsInDreams;
+		bool mExistsInNightmares;
+
+		bool existsInDreams() const;
+		void setExistsInDreams(bool existsInDreams);
+
+		bool existsInNightmares() const;
+		void setExistsInNightmares(bool existsInNightmares);
+
+		int getState() const;
+		void setState(int state);
+
+		int getHealthPoints() const;
+		void setHealthPoints(int healthPoints);
+
+		int getNumLives() const;
+		void setNumLives(int numLives);
+		
+		std::string getScriptFilename() const;
+		void setScriptFilename(const std::string& scriptFilename);
 	};
-
+	
 	class TLogicComponentParameters: public TComponentParameters
 	{
 	public:
 		TLogicComponentParameters();
 		~TLogicComponentParameters();
+
+		/// Existence in each world
+		bool existsInDreams;
+		bool existsInNightmares;
 	};
 }
 
