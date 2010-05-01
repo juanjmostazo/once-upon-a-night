9041b6ddd8ab31fd1e92614e7d2ce072db64ad17
juanj.mostazo <juanj.mostazo@1610d384-d83c-11de-a027-019ae363d039>
2010-05-01 15:46:16 +0000

    Added ony's landing particle system (from state jump to state idle). Scaled portal's change world particle system.

--- OUAN/OUAN/Src/Graphics/ParticleManager/ParticleTemplates.cpp
+++ OUAN/OUAN/Src/Graphics/ParticleManager/ParticleTemplates.cpp
@@ -34,12 +34,14 @@ bool ParticleTemplates::loadConfig()
 	if (config.loadFromFile(PARTICLES_CFG))
 	{
 		config.getOption("PORTAL_CHANGE_WORLD", PORTAL_CHANGE_WORLD); 
+		config.getOption("ONY_LAND", ONY_LAND); 
 
 		success = true;
 	}
 	else
 	{
 		PORTAL_CHANGE_WORLD="";
+		ONY_LAND="";
 
 		success = false;
 	}
