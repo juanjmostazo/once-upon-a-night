0ebd94647828091f0e67c7a1f55b7f8df852efc1
ithiliel <ithiliel@1610d384-d83c-11de-a027-019ae363d039>
2010-05-15 18:34:30 +0000

    - Intro State added. It will play a video unless the app can't find the file. In that case, an image will be displayed for a fixed amount of time. Both the playback and the image will stop from showing (and the game will advance to the Main Menu State) if an alphanumeric or Escape key are pressed or if the left/right mouse buttons are clicked. - Video player module added.

---
+++ trunk/OUAN/OUAN/Src/Core/IntroState.h
@@ -0,0 +1,59 @@
+#ifndef INTROSTATEH_H
+#define INTROSTATEH_H
+#include "../OUAN.h"
+#include "GameState.h"
+
+namespace OgreUtils
+{
+	class DirectShowMovieTexture;
+}
+namespace OUAN
+{
+	const std::string INTRO_MOVIE="Onceuponanight.avi";	
+	const std::string VIDEO_PATH="../../Resources/Graphics/Video/";
+
+	//Default image to show when the video file has not been found
+	const std::string DEFAULT_IMG="ouan-nologo-bg.png";
+	const std::string MOVIE_MATERIAL_NAME="Movie";
+	const std::string MOVIE_MATERIAL_GROUP="General";
+	const std::string SCREEN_SCENE_NODE="Screen";
+	
+	const int IMAGE_DELAY=5;//time in seconds the fallback image will be displayed
+
+
+	///State corresponding to the game's main menu
+	class IntroState: public GameState, public boost::enable_shared_from_this<IntroState>{
+	private:
+		OgreUtils::DirectShowMovieTexture* mMovieTexture;
+		Ogre::Rectangle2D* mScreen;
+		int mDelay;
+
+	public:
+		/// init main menu's resources
+		void init(ApplicationPtr app);
+
+		/// Clean up main menu's resources
+		void cleanUp();
+
+		/// pause state
+		void pause();
+		/// resume state
+		void resume();
+
+		/// process input events
+		/// @param app	the parent application
+		void handleEvents();
+		/// Update game according to the current state
+		/// @param app	the parent app
+		void update(long elapsedTime);
+		bool keyPressed( const OIS::KeyEvent& e );
+		bool mousePressed(const OIS::MouseEvent &e, OIS::MouseButtonID id);
+
+		/// Default constructor
+		IntroState();
+		/// Destructor
+		~IntroState();
+
+	};
+}
+#endif
