1bddf2b9b77028f4244cacfab10097ad51dac008
juanj.mostazo <juanj.mostazo@1610d384-d83c-11de-a027-019ae363d039>
2010-06-27 20:31:03 +0000

    Added code to make visible portal artifact when all tripollos in platform big are killed

--- trunk/OUAN/OUAN/Src/Logic/LogicComponent/LogicComponentActionTrigger.cpp
+++ trunk/OUAN/OUAN/Src/Logic/LogicComponent/LogicComponentActionTrigger.cpp
@@ -5,6 +5,8 @@
 #include "../../Application.h"
 #include "../../Game/GameWorldManager.h"
 #include "../../Game/GameObject/GameObject.h"
+#include "../../Game/GameObject/GameObjectTripolloDreams.h"
+#include "../../Game/GameObject/GameObjectPortal.h"
 #include "../../Event/EventDefs.h"
 
 using namespace OUAN;
@@ -21,9 +23,11 @@ LogicComponentActionTrigger::~LogicComponentActionTrigger()
 
 void LogicComponentActionTrigger::processCollision(GameObjectPtr pGameObject, Ogre::Vector3 pNormal)
 {
+	std::string triggerPlatformBigName = "trigger-action#platform_big";
+
 	if(pGameObject->getType().compare(GAME_OBJECT_TYPE_ONY)==0)
 	{
-		if (getParent()->getName().compare("trigger-action#platform_big")==0)
+		if (getParent()->getName().compare(triggerPlatformBigName)==0)
 		{
 			processActionPlatformBig();
 		}
@@ -42,7 +46,88 @@ void LogicComponentActionTrigger::processExitTrigger(GameObjectPtr pGameObject)
 
 void LogicComponentActionTrigger::processActionPlatformBig()
 {
-	//Logger::getInstance()->log("@@ PROCESS ACTION PLATFORM BIG WITH ONY");
+	Logger::getInstance()->log("@@ PROCESS ACTION PLATFORM BIG WITH ONY");
+
+	///////////////////
+
+	GameObjectPortalPtr portalPlatformBig;
+	bool portalPlatformBigFound = false;
+
+	std::string portalPlatformBigName = "changeworld#platform_big";
+	double maxDistanceToPortal = 300;
+
+	bool allTripollosAreDisabled = true;
+
+	///////////////////
+
+	TGameObjectPortalContainer::iterator itPortal;
+	TGameObjectPortalContainer * containerPortal = 
+		Application::getInstance()->getGameWorldManager()->getGameObjectPortalContainer();
+
+	for(itPortal = containerPortal->begin(); itPortal != containerPortal->end(); itPortal++)
+	{
+		GameObjectPortalPtr portal = (*itPortal);
+
+		if (portal->getName().compare(portalPlatformBigName) == 0)
+		{
+			portalPlatformBig = portal;
+			portalPlatformBigFound = true;
+			break;
+		}
+	}
+
+	///////////////////
+
+	if (!portalPlatformBigFound)
+	{
+		Logger::getInstance()->log("LogicComponentActionTrigger::processActionPlatformBig() [ERROR]: portalPlatformBig not found");
+	}
+
+	///////////////////
+
+	TGameObjectTripolloDreamsContainer::iterator itTripollo;
+	TGameObjectTripolloDreamsContainer * containerTripollo = 
+		Application::getInstance()->getGameWorldManager()->getGameObjectTripolloDreamsContainer();
+
+	for(itTripollo = containerTripollo->begin(); itTripollo != containerTripollo->end(); itTripollo++)
+	{
+		GameObjectTripolloDreamsPtr tripollo = (*itTripollo);
+
+		//TODO FIX THIS HACK
+		if (!tripollo.get())
+		{
+			continue;
+		}
+
+		double distanceToPortal = tripollo->getRenderComponentPositional()->getPosition().distance(
+			portalPlatformBig->getRenderComponentPositional()->getPosition());
+
+		Logger::getInstance()->log("@@ " + tripollo->getName() + " -> " + Ogre::StringConverter::toString(Ogre::Real(distanceToPortal)));
+
+		if (distanceToPortal <= maxDistanceToPortal)
+		{
+			Logger::getInstance()->log("@@ " + tripollo->getName() + " -> IN! -> " + Ogre::StringConverter::toString(tripollo->isEnabled()));	
+
+			if (tripollo->isEnabled())
+			{
+				allTripollosAreDisabled = false;
+				break;
+			}
+		}
+	}
+
+	///////////////////
+
+	if (allTripollosAreDisabled)
+	{
+		Logger::getInstance()->log("@@ ALL TRIPOLLOS ARE DISABLED");
+		portalPlatformBig->getEntityComponent()->setVisible(true);		
+	}
+	else
+	{
+		Logger::getInstance()->log("@@ AT LEAST ONE TRIPOLLO IS ENABLED");
+		portalPlatformBig->getEntityComponent()->setVisible(false);
+	}
 }
 
 TLogicComponentActionTriggerParameters::TLogicComponentActionTriggerParameters() : TLogicComponentParameters()
