9985872731b225c75674561abaae75adc672c61f
wyern1 <wyern1@1610d384-d83c-11de-a027-019ae363d039>
2010-08-18 14:22:34 +0000

    cutscenes and checkpoints bug

--- OUAN/Resources/Scripts/checkpoints.lua
+++ OUAN/Resources/Scripts/checkpoints.lua
@@ -1,120 +1,120 @@
 -- CHECKPOINT 1: AFTER FIRST BRIDGE
 function enterCheckPoint1(pOny)
-	log ("enterCheckPoint1")
+	log ("enterCheckPoint1");
 	setCheckPoint("CheckPoint1",1);
 return
 end
 
 function conditionCheckPoint1(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<1
+	return checkpointNumber<1;
 end
 
 -- CHECKPOINT 2: BOMBS PUZZLE START
 function enterCheckPoint2(pOny)
-	log ("enterCheckPoint2")
+	log ("enterCheckPoint2");
 	setCheckPoint("CheckPoint2",2);
 return
 end
 
 function conditionCheckPoint2(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<2
+	return checkpointNumber<2;
 end
 
 -- CHECKPOINT 3: BOMBS PUZZLE END
 function enterCheckPoint3(pOny)
-	log ("enterCheckPoint3")
+	log ("enterCheckPoint3");
 	setCheckPoint("CheckPoint3",3);
 return
 end
 
 function conditionCheckPoint3(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<3
+	return checkpointNumber<3;
 end
 
 -- CHECKPOINT 4: NOT USED!!!
 function enterCheckPoint4(pOny)
-	log ("enterCheckPoint4")
+	log ("enterCheckPoint4");
 	setCheckPoint("CheckPoint4",4);
 return
 end
 
 function conditionCheckPoint4(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<4
+	return checkpointNumber<4;
 end
 
 -- CHECKPOINT 5: NIGHT GOBLIN APPEARS / DEFEATED
 function enterCheckPoint5(pOny)
-	log ("enterCheckPoint5")
+	log ("enterCheckPoint5");
 	setCheckPoint("CheckPoint5",5);
 return
 end
 
 function conditionCheckPoint5(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<5
+	return checkpointNumber<5;
 end
 
 -- CHECKPOINT 6: TRIPOLLO'S PLATFORM COMPLETED
 function enterCheckPoint6(pOny)
-	log ("enterCheckPoint6")
+	log ("enterCheckPoint6");
 	setCheckPoint("CheckPoint6",6);
 return
 end
 
 function conditionCheckPoint6(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<6
+	return checkpointNumber<6;
 end
 
 -- CHECKPOINT 7: TENTETIESOS
 function enterCheckPoint7(pOny)
-	log ("enterCheckPoint7")
+	log ("enterCheckPoint7");
 	setCheckPoint("CheckPoint7",7);
 return
 end
 
 function conditionCheckPoint7(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<7
+	return checkpointNumber<7;
 end
 
--- CHECKPOINT 8:
+-- CHECKPOINT 8: TOWER 3 ISLAND 2
 function enterCheckPoint8(pOny)
-	log ("enterCheckPoint8")
+	log ("enterCheckPoint8");
 	setCheckPoint("CheckPoint8",8);
 return
 end
 
 function conditionCheckPoint8(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<8
+	return checkpointNumber<8;
 end
 
--- CHECKPOINT 9:
+-- CHECKPOINT 9: FINAL BOSS
 function enterCheckPoint9(pOny)
-	log ("enterCheckPoint9")
+	log ("enterCheckPoint9");
 	setCheckPoint("CheckPoint9",9);
 return
 end
 
 function conditionCheckPoint9(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<9
+	return checkpointNumber<9;
 end
 
--- CHECKPOINT 10:
+-- CHECKPOINT 10: NOT USED
 function enterCheckPoint10(pOny)
-	log ("enterCheckPoint10")
+	log ("enterCheckPoint10");
 	setCheckPoint("CheckPoint10",10);
 return
 end
 
 function conditionCheckPoint10(pOny)
     local checkpointNumber=getCheckPointNumber();
-	return checkpointNumber<10
+	return checkpointNumber<10;
 end
 
